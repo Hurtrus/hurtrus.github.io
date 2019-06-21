69fccde11349d3226f636dcd270a7aed7b6bc8d9
Scott Hurst <Hurtrus@users.noreply.github.com>
2019-06-21 13:32:42 -0400

    testing
    test

---
+++ Stream Resources/SceneMaker/Settings/Starting - settings.js
@@ -0,0 +1,10 @@
+var settings = {
+    "options": { "backgroundType": "image", "backgroundOverlayOpacity": 0.8, "backgroundBlur": "4", "backgroundScale": 1, "displayBranding": "yes", "logoOpacity": "1", "logoScale": "2", "frameWidth": "0", "sceneTitle": "Hurtrus", "tagline": "THE STREAM IS ABOUT TO BEGIN" },
+    "colors": { "backgroundOverlay": "rgba(54, 53, 58, 1)", "accentColor": "rgba(234, 220, 4, 1)", "frameColor": "rgba(189,27,20,1)", "primaryTextColor": "rgba(255, 255, 255, 1)", "subTextColor": "rgba(234, 220, 4, 1)", "contentBackgrounds": "rgba(255, 255, 255, .1)" },
+    "labels": { "displayLabels": "yes" },
+    "schedule": { "displaySchedule": "yes", "sunday": "9:30p - 11:30p EST", "monday": "9:30p - 11:30p EST", "tuesday": "9:30p - 11:30p EST", "wednesday": "9:30p - 11:30p EST", "thursday": "9:30p - 11:30p EST", "friday": "9:30p - 11:30p EST", "saturday": "9:30p - 11:30p EST" },
+    "scaling": { "socialMediaScale": 1, "labelsScale": 1, "scheduleScale": 1, "countdownScale": 1 },
+    "fonts": { "primaryFont": "Rubik Mono One", "subFont": "Racing Sans One", "countDownFont": "ABeeZee", "titleSize": "200", "titleVerticalOffset": "-103", "subtitleSize": "124", "subtitleVerticalOffset": "-83", "labelNameSize": 20, "labelNameVerticalOffset": 0, "labelHeaderSize": 12, "labelHeaderVerticalOffset": 0, "countdownTimeSize": 60, "countdownTimeVerticalOffset": 0, "countdownMessageSize": 24, "countdownMessageVerticalOffset": "-20", "countdownEndMessageSize": 60, "countdownEndMessageVerticalOffset": 0 },
+    "countdown": { "displayCountdown": "yes", "countdownTime": "5", "countdownMessage": "until the stream starts!", "countdownOverMessage": "Let's go!" },
+    "social": { "displaySocial": "yes", "twitch": "Hurtrus", "twitterHeader": "Follow Me On Twitter", "twitter": "@Hurtrus_Streaming", "facebookHeader": "", "facebook": "", "instagramHeader": "", "instagram": "", "youtubeHeader": "Subscribe On YouTube", "youtube": "HurtrusStreaming" }
+};
